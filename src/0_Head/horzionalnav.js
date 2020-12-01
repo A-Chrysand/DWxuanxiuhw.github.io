59c7b300459b2a1013c3a5496357199957ae4a58
A-Chrysand <56879149+A-Chrysand@users.noreply.github.com>
2020-12-01 17:55:26 +0800

    1

--- src/0_Head/horzionalnav.js
+++ src/0_Head/horzionalnav.js
@@ -1,5 +1,5 @@
 /*---------------------全局设置---------------------*/
-var nav_ul_width= 80;					//设置导航栏总宽度
+var nav_ul_width = 80;					//设置导航栏总宽度
 var bol = 1;							//设置1向上翻 -1向下翻
 var height = 36;						//设置导航栏高度
 var deg = 90;							//设置翻转角度
@@ -7,8 +7,8 @@ var donghuashijian = 0.1;				//设置动画时间
 var frontcolor = "rgb(100,200,100)";	//设置滑块前面颜色
 var backcolor = "pink";					//设置滑块背面颜色
 /*---------------------全局设置---------------------*/
-document.documentElement.style.setProperty('--nvwid', nav_ul_width+"%");
-document.documentElement.style.setProperty('--nvmarginleft', (100-nav_ul_width)/2+"%");
+document.documentElement.style.setProperty('--nvwid', nav_ul_width + "%");
+document.documentElement.style.setProperty('--nvmarginleft', (100 - nav_ul_width) / 2 + "%");
 document.documentElement.style.setProperty('--hei', height);
 document.documentElement.style.setProperty('--dg', deg);
 document.documentElement.style.setProperty('--donghuashijian', donghuashijian);
@@ -37,8 +37,8 @@ window.onload = function () {
         hz_nav_li[i].index = i;
         hz_nav_li[i].onclick = function () {
             for (var n = 0; n < len; n++) {
-                hz_nav_li[n].className = "";
-                hz_nav_iframe[n].className = "hz_hiddenone";
+                hz_nav_li[n].className = "hz_nav_li_showone";
+                hz_nav_iframe[n].className = "hz_nav_li_hiddenone";
             }
             this.className = "hz_nav_li";
             hz_nav_iframe[this.index].className = "";
@@ -47,4 +47,16 @@ window.onload = function () {
 }
 
 
+function reinitIframe() {
+    var iframe = document.getElementById("hz_nav_li_showone");
+    try {
+        var bHeight = iframe.contentWindow.document.body.scrollHeight;
+        /*       
+                           var dHeight = iframe.contentWindow.document.documentElement.scrollHeight;              
+                           var height = Math.max(bHeight, dHeight);                          
+                            iframe.height = height;                */
+        iframe.height = bHeight;
+    }
+    catch (ex) { }
+}
 
